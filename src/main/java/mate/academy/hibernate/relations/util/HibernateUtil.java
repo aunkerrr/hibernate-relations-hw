f2c9706c8d257ebd22eb9ce50c5246932ea76079
Nikita Myroniuk <nikitamix1sstt@gmail.com>
2026-07-15 14:43:21 +0200

    working but there is LazyInitialization which is going to be figured out using hql

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -1,9 +1,21 @@
 package mate.academy.hibernate.relations.util;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            sessionFactory =
+                    new Configuration().configure().buildSessionFactory();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't create SessionFactory", e);
+        }
+    }
+
     public static SessionFactory getSessionFactory() {
-        return null;
+        return sessionFactory;
     }
 }
